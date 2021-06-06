284edd681505fc18f9c10454edc92d550a27bfbd
Marnix Enthoven <26323484+narbux@users.noreply.github.com>
2021-06-06 11:56:36 +0200

    Changed logging format

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
     ch = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     logger.addHandler(ch)
     try:
